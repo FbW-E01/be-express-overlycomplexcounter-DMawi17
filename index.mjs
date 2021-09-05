41d464d752fc6f243df89f83025b65026aa1ed17
DMawi17 <asegda17@gmail.com>
2021-09-05 20:56:16 +0200

    HTTP server closed

--- index.mjs
+++ index.mjs
@@ -5,9 +5,11 @@ const port = 8000;
 
 let counter = 0;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server on localhost:${port}`);
 });
+
+//...
 
 app.get("/", (req, res) => {
     counter;
@@ -21,18 +23,16 @@ app.post("/inc", (req, res) => {
     res.send(`Count: ${counter}`);
 });
 
-//..
+//...
 
 app.post("/dec", (req, res) => {
     counter--;
     res.send(`Count: ${counter}`);
 });
 
-//..
+//...
 
 app.post("/die", (req, res) => {
-    res.send("shuting down!");
-    process.exit;
+    res.send("signal received: closing HTTP server");
+    server.close(() => console.log("HTTP server closed"));
 });
-
-// ...
